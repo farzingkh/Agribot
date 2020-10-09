a1cafb7b4f8725e005d8ac94f78cc0aa5641fbcd
farzingkh <farzin.golkhosh@gmail.com>
2020-10-09 05:41:52 +0000

    [FEAT] Update pick_objects

--- catkin_ws/src/pick_objects/src/pick_objects.cpp
+++ catkin_ws/src/pick_objects/src/pick_objects.cpp
@@ -20,12 +20,14 @@ int main(int argc, char** argv){
   move_base_msgs::MoveBaseGoal goal;
 
   // set up the frame parameters
-  goal.target_pose.header.frame_id = "map";
+  goal.target_pose.header.frame_id = "robot_footprint";
   goal.target_pose.header.stamp = ros::Time::now();
 
   // Define a position and orientation for the robot to reach
   goal.target_pose.pose.position.x = 5.0;
+  goal.target_pose.pose.position.y = 0.0;
   goal.target_pose.pose.orientation.w = 1.0;
+
 
    // Send the goal position and orientation for the robot to reach
   ROS_INFO("Sending pick up point");
@@ -36,16 +38,17 @@ int main(int argc, char** argv){
 
   // Check if the robot reached its goal
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hooray, the robot reached it's goal!");
+    ROS_INFO("The robot reached its goal!");
   else
-    ROS_INFO("The base failed to move forward 1 meter for some reason");
+    ROS_INFO("The base failed to reach its goal!");
 
   // wait for 5 seconds
   ros::Duration(5).sleep();
 
   // Define a second position and orientation for the robot to reach for drop off
-  goal.target_pose.pose.position.x = 2.0;
-  goal.target_pose.pose.orientation.w = 0.0;
+  goal.target_pose.pose.position.x = -5.0;
+  goal.target_pose.pose.position.y = -1.0;
+  goal.target_pose.pose.orientation.w = 1.0;
 
   // Send the goal position and orientation for the robot to reach
   ROS_INFO("Sending drop off point");

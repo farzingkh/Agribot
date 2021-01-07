4cfc2193942d98dbc05481c0fc060b1ca394b030
root <farzin.golkhosh@gmail.com>
2021-01-07 16:52:06 +0000

    [FIX] Update marker test

--- catkin_ws/src/scripts/add_markers.sh
+++ catkin_ws/src/scripts/add_markers.sh
@@ -4,6 +4,6 @@ sleep 5
 xterm -e " home/workspace/Service_Robot/catkin/devel/setup.bash; roslaunch robot world.launch" &
 sleep 5 
 xterm -e " home/workspace/Service_Robot/catkin/devel/setup.bash; roslaunch amcl_localization amcl.launch" &
-sleep 5 
-xterm -e " home/workspace/Service_Robot/catkin/devel/setup.bash; rosrun add_markers add_markers"
+sleep 10 
+xterm -e " home/workspace/Service_Robot/catkin/devel/setup.bash; rosrun add_markers add_markers_node test"
 
